6f6eeee9fffe30e0a83377b8201e883c85518f18
imdelacruz03 <imdelacruz@ucsd.edu>
2026-04-17 16:23:47 -0700

    updated js

--- global.js
+++ global.js
@@ -4,6 +4,11 @@ function $$(selector, context = document){
     return Array.from(context.querySelectorAll(selector));
 }
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"
+    : "/Portfolio/";
+
+
 // Below is a data structure, and it's storing the information
 // on all of the pages in one place so the code below can use it
 // this is so we don't have to had a nav on every html
@@ -23,8 +28,25 @@ document.body.prepend(nav); // inserts into the page at the very top of the <bod
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+    // fix url with BASE_PATH if it's an internal link
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    // highlight current page
+    a.classList.toggle('current', a.host === location.host && a.pathname === location.pathname);
+
+    // open external links in a new tab
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
+
+    nav.append(a);
 }
+
 
 // creates an array of all the nav links on whichever page is 
 // currently loaded in the braoswer at that moment
@@ -40,6 +62,6 @@ for (let p of pages) {
 // if navLinks can't find any matching links it returns underfined
 // instead of an element therefore when .classList.add() is called the 
 // whole script will crash with errors
-//currentLink?.classList.add('current');
+currentLink?.classList.add('current');
 
 // currentLink? means "only continue if this isn't undefined"
